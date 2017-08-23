8878fa11f261144d31befb1f5cedcc1394be834c
ClarkPeng <Clark.Peng@activenetwork.com>
2017-08-23 21:25:50 +0800

    Add page object ContactSubmitToCompliance

---
+++ Craftsman/JumpForward.Common/PageObject/Coach/Compliance/ContactSubmitToCompliance.cs
@@ -0,0 +1,18 @@
+﻿using Craftsman.Core.Utilities;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JumpForward.Common.PageObject.Coach.Compliance
+{
+    public class ContactSubmitToCompliance: CoachPageBase
+    {
+        public ContactSubmitToCompliance(IWebDriver driver) : base(driver)
+        {
+            WaitSelector.WaitingFor_ElementExists(this.Driver, By.Id(""));
+        }
+    }
+}
